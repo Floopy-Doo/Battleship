b4e7b97c1cd635ec6e5ef56a5d37d204a1ddb6f8
Florian Bucher <florian.bucher@hotmail.com>
2015-05-06 23:45:31 +0200

    Untested stuff Activities done

--- app/src/main/java/ch/hslu/mpbro15/team10/battleship/MatchingActivity.java
+++ app/src/main/java/ch/hslu/mpbro15/team10/battleship/MatchingActivity.java
@@ -43,7 +43,7 @@ import ch.hslu.mpbro15.team10.battleship.BaseGame.BaseGameUtils;
  */
 public class MatchingActivity extends Activity
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        RealTimeMessageReceivedListener, RoomStatusUpdateListener, RoomUpdateListener,
+        RealTimeMessageReceivedListener,
         OnInvitationReceivedListener {
         /*
      * API INTEGRATION SECTION. This section contains the code that integrates
@@ -439,38 +439,8 @@ public class MatchingActivity extends Activity
         switchToScreen(R.id.screen_sign_in);
     }
 
-    // Called when we are connected to the room. We're not ready to play yet! (maybe not everybody
-    // is connected yet).
-    @Override
-    public void onConnectedToRoom(Room room) {
-        Log.d(TAG, "onConnectedToRoom.");
 
-        // get room ID, participants and my ID:
-        mRoomId = room.getRoomId();
-        mParticipants = room.getParticipants();
-        mMyId = room.getParticipantId(Games.Players.getCurrentPlayerId(mGoogleApiClient));
 
-        // print out the list of participants (for debug purposes)
-        Log.d(TAG, "Room ID: " + mRoomId);
-        Log.d(TAG, "My ID " + mMyId);
-        Log.d(TAG, "<< CONNECTED TO ROOM>>");
-    }
-
-    // Called when we've successfully left the room (this happens a result of voluntarily leaving
-    // via a call to leaveRoom(). If we get disconnected, we get onDisconnectedFromRoom()).
-    @Override
-    public void onLeftRoom(int statusCode, String roomId) {
-        // we have left the room; return to main screen.
-        Log.d(TAG, "onLeftRoom, code " + statusCode);
-        switchToMainScreen();
-    }
-
-    // Called when we get disconnected from the room. We return to the main screen.
-    @Override
-    public void onDisconnectedFromRoom(Room room) {
-        mRoomId = null;
-        showGameError();
-    }
 
     // Show error message about game being cancelled and return to main screen.
     void showGameError() {
@@ -478,96 +448,8 @@ public class MatchingActivity extends Activity
         switchToMainScreen();
     }
 
-    // Called when room has been created
-    @Override
-    public void onRoomCreated(int statusCode, Room room) {
-        Log.d(TAG, "onRoomCreated(" + statusCode + ", " + room + ")");
-        if (statusCode != GamesStatusCodes.STATUS_OK) {
-            Log.e(TAG, "*** Error: onRoomCreated, status " + statusCode);
-            showGameError();
-            return;
-        }
 
-        // show the waiting room UI
-        showWaitingRoom(room);
-    }
 
-    // Called when room is fully connected.
-    @Override
-    public void onRoomConnected(int statusCode, Room room) {
-        Log.d(TAG, "onRoomConnected(" + statusCode + ", " + room + ")");
-        if (statusCode != GamesStatusCodes.STATUS_OK) {
-            Log.e(TAG, "*** Error: onRoomConnected, status " + statusCode);
-            showGameError();
-            return;
-        }
-        updateRoom(room);
-    }
-
-    @Override
-    public void onJoinedRoom(int statusCode, Room room) {
-        Log.d(TAG, "onJoinedRoom(" + statusCode + ", " + room + ")");
-        if (statusCode != GamesStatusCodes.STATUS_OK) {
-            Log.e(TAG, "*** Error: onRoomConnected, status " + statusCode);
-            showGameError();
-            return;
-        }
-
-        // show the waiting room UI
-        showWaitingRoom(room);
-    }
-
-    // We treat most of the room update callbacks in the same way: we update our list of
-    // participants and update the display. In a real game we would also have to check if that
-    // change requires some action like removing the corresponding player avatar from the screen,
-    // etc.
-    @Override
-    public void onPeerDeclined(Room room, List<String> arg1) {
-        updateRoom(room);
-    }
-
-    @Override
-    public void onPeerInvitedToRoom(Room room, List<String> arg1) {
-        updateRoom(room);
-    }
-
-    @Override
-    public void onP2PDisconnected(String participant) {
-    }
-
-    @Override
-    public void onP2PConnected(String participant) {
-    }
-
-    @Override
-    public void onPeerJoined(Room room, List<String> arg1) {
-        updateRoom(room);
-    }
-
-    @Override
-    public void onPeerLeft(Room room, List<String> peersWhoLeft) {
-        updateRoom(room);
-    }
-
-    @Override
-    public void onRoomAutoMatching(Room room) {
-        updateRoom(room);
-    }
-
-    @Override
-    public void onRoomConnecting(Room room) {
-        updateRoom(room);
-    }
-
-    @Override
-    public void onPeersConnected(Room room, List<String> peers) {
-        updateRoom(room);
-    }
-
-    @Override
-    public void onPeersDisconnected(Room room, List<String> peers) {
-        updateRoom(room);
-    }
 
     void updateRoom(Room room) {
         if (room != null) {
